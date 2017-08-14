1985c0f098ecdecb17cc1865752c4599528707df
git push -u origin <mazairaj94@gmail.com>
2017-08-14 13:21:27 -0700

    eh

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/app';
 
 ReactDOM.render(
-  <BrowserRouter np>
+  <BrowserRouter >
     <App />
   </BrowserRouter>,
     document.getElementById('root'),
